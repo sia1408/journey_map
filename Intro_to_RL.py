26d03968c122b6b0fad1ed450c1d0b4fc7659c7e
sia1408 <85342899+sia1408@users.noreply.github.com>
2025-04-01 17:27:04 +0530

    Update Intro_to_RL.py

--- Intro_to_RL.py
+++ Intro_to_RL.py
@@ -74,9 +74,11 @@ st.markdown("""
 Let's watch a Reinforcement Learning agent learn how to play Blackjack through **Q-learning**. 
 
 ### 🎛️ Interactive Parameters
-Open the sidebar by clicking the **›** icon to the left.
+Open the sidebar by clicking the **›** icon to the left.""")
+st.markdown("""
 Adjust these parameters to see how they impact the agent's learning:
-
+""")
+st.markdown("""
     - **Episodes**: In reinforcement learning, an episode is a complete sequence of steps taken by an agent—from the initial state to a terminal state (like winning, losing, or reaching a time limit).
     It’s one full trial where the agent interacts with the environment, makes decisions, receives rewards, and learns from the outcome.
     Think of it as a single trial-and-error run, where the agent tries a strategy, sees how it goes, and uses that experience to improve next time.""")
